bf1ffa232b73fdf355d5332f9e669d98e26b5a92
shsha0110 <shsh0201@snu.ac.kr>
2025-11-05 17:08:31 +0900

    fix(InputArea): Correctly hide fetch button to allow input to fill width

--- aiblog-client/src/components/RepoSetup/InputArea/index.jsx
+++ aiblog-client/src/components/RepoSetup/InputArea/index.jsx
@@ -1,12 +1,21 @@
-import { Box } from '@mui/material';
+import { Box, Collapse } from '@mui/material';
 import { RepoInput } from './RepoInput';
 import { FetchButton } from './FetchButton';
+import { useAppContext } from '@/contexts/AppContext';
 
 export function InputArea() {
+  const { repoName } = useAppContext();
+
   return (
     <Box sx={{ display: 'flex', alignItems: 'flex-start', gap: 2 }}>
-      <RepoInput />
-      <FetchButton />
+      <Box sx={{ flexGrow: 1 }}>
+        <RepoInput />
+      </Box>
+      {repoName && (
+        <Collapse in={!!repoName}>
+          <FetchButton />
+        </Collapse>
+      )}
     </Box>
   );
 }
