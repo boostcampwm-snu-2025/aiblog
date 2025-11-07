89ba0cf38beb8cd9c86a0ebb4d227df9b2eb86be
kreatinj <kjune1999@naver.com>
2025-11-08 02:51:45 +0900

    feat(server): install zod and validate env

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,11 +3,18 @@ import { fileURLToPath } from "node:url";
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
+import z from "zod";
 
 dotenv.config();
 
+const env = z
+	.object({
+		PORT: z.string().default("3001"),
+		GITHUB_TOKEN: z.string(),
+	})
+	.parse(process.env);
+
 const app = express();
-const PORT = 3001;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -16,6 +23,6 @@ app.use(express.static(path.join(__dirname, "../../web/dist")));
 app.use(cors());
 app.use(express.json());
 
-app.listen(PORT, () => {
-	console.log(` Server on http://localhost:${PORT}`);
+app.listen(env.PORT, () => {
+	console.log(` Server on http://localhost:${env.PORT}`);
 });
