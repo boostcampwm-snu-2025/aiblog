2723aca95edd00d012395c065a14ea26762e2309
kreatinj <kjune1999@naver.com>
2025-11-08 01:39:17 +0900

    chore(server): biome check

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,7 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 import cors from "cors";
 import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
 
 const app = express();
 const PORT = 3001;
@@ -14,5 +14,5 @@ app.use(cors());
 app.use(express.json());
 
 app.listen(PORT, () => {
-  console.log(` Server on http://localhost:${PORT}`);
+	console.log(` Server on http://localhost:${PORT}`);
 });
