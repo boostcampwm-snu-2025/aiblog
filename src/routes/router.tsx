c5721bc2fa4fb9a06c5afc7c411945dab9a8c9e7
lchanss <lchans0319@gmail.com>
2025-11-08 18:28:19 +0900

    feat: NotFound 페이지 추가

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
 
 import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
   },
 ]);
 
