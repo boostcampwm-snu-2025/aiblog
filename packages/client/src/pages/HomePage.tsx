54eb7f866ec76d8e869b84eb7d3d6311b4a02cf4
kimnamheeee <nami200284@snu.ac.kr>
2025-11-09 16:33:38 +0900

    ✨ feat(pages): add pages component
    home / settings / savedposts

---
+++ packages/client/src/pages/HomePage.tsx
@@ -0,0 +1,7 @@
+export function HomePage() {
+  return (
+    <div className="space-y-4">
+      <h1 className="text-3xl font-bold text-gray-900">Home</h1>
+    </div>
+  );
+}
---
+++ packages/client/src/pages/SavedPostsPage.tsx
@@ -0,0 +1,7 @@
+export function SavedPostsPage() {
+  return (
+    <div className="space-y-4">
+      <h1 className="text-3xl font-bold text-gray-900">Saved Posts</h1>
+    </div>
+  );
+}
